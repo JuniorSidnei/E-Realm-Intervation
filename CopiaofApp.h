f47e9746e1321e388c2202c7b38802d94b05028e
JuniorSidnei <sidneicarrarojunior@gmail.com>
2016-11-09 22:19:59 -0200

    Add files via upload

---
+++ CopiaofApp.h
@@ -0,0 +1,397 @@
+#include "ofApp.h"
+
+void ofApp::setup()
+{
+
+
+	//vida da HUD
+	player.lifeBarPlayer.loadImage("players/BarraVida.png");
+	player.lifePlayer.loadImage("players/Vida.png");
+	player.lifeBarPlayer.resize(305, 80);
+	player.lifePlayer.resize(2, 80);
+
+	//Carregando as imagens de HUD dos inimigos
+	for (int i = 0; i < Ninimigo; i++)
+	{
+		inimigo[i].EnemyBar.loadImage("players/LifeEnemyBar.png");
+		inimigo[i].EnemyLife.loadImage("players/LifeEnemy.png");
+		inimigo[i].EnemyBar.resize(100, inimigo[i].EnemyBar.getHeight());
+		inimigo[i].EnemyLife.resize(14, inimigo[i].EnemyLife.getHeight());
+	}
+
+
+	//Imagem de fundo
+	fundo.posicao.x = 0;
+	fundo.posicao.y = 0;
+	fundo.sprite.loadImage("Cenario/Rua2.png");
+	FundoMenu.loadImage("Menu/FundoMenu.png");
+
+
+	//definicoes padrao do player
+	player.posicao.x = 600;
+	player.posicao.y = 400;
+	player.sprite.loadImage("players/Player.png");
+	player.tamanhoY = player.sprite.getHeight() / 2;
+	player.tamanhoX = player.sprite.getWidth() / 2;
+	player.tamanhoXLife = player.lifePlayer.getWidth() / 2;
+	player.tamanhoYLife = player.lifePlayer.getHeight() / 2;
+	player.sprite.setAnchorPoint(player.sprite.getWidth() / 2, player.sprite.getHeight() / 2);
+	player.acele = 20.0f;
+	player.vida = 300;
+
+	//definicoes powerUp de dano
+	damageUp.damage.loadImage("players/PowerUpForca.png");
+	damageUp.damage.setAnchorPoint(damageUp.damage.getWidth() / 2, damageUp.damage.getHeight() / 2);
+	damageUp.tamanhoY = damageUp.damage.getHeight() / 2;
+	damageUp.tamanhoX = damageUp.damage.getWidth() / 2;
+
+	//definicoes da cura
+	potion.heal.loadImage("players/PocaoVida.png");
+	potion.heal.setAnchorPoint(potion.heal.getWidth() / 2, potion.heal.getHeight() / 2);
+	potion.tamanhoY = potion.heal.getHeight() / 2;
+	potion.tamanhoX = potion.heal.getWidth() / 2;
+
+	//definicoes padrao dos inimigos por vetores
+	for (int i = 0; i < Ninimigo; i++)
+	{
+
+		inimigo[i].posicao.x = 900 + (rand() % 4072);
+		inimigo[i].posicao.y = 100 + (rand() % 450);
+		inimigo[i].sprite.loadImage("players/AlienVerde.png");
+		inimigo[i].sprite2.loadImage("players/AlienAmarelo.png");
+		inimigo[i].sprite3.loadImage("players/AlienVermelho.png");
+		inimigo[i].tamanhoY = inimigo[i].sprite.getHeight() / 2;
+		inimigo[i].tamanhoX = inimigo[i].sprite.getWidth() / 2;
+		inimigo[i].tamanhoXLife = inimigo[i].EnemyLife.getWidth();
+		inimigo[i].tamanhoYLife = inimigo[i].EnemyLife.getHeight();
+		inimigo[i].sprite.setAnchorPoint(inimigo[i].sprite.getWidth() / 2, inimigo[i].sprite.getHeight() / 2);
+		inimigo[i].sprite2.setAnchorPoint(inimigo[i].sprite2.getWidth() / 2, inimigo[i].sprite2.getHeight() / 2);
+		inimigo[i].sprite3.setAnchorPoint(inimigo[i].sprite3.getWidth() / 2, inimigo[i].sprite3.getHeight() / 2);
+		inimigo[i].acele = 1.0f;
+		inimigo[i].IniSub = true;
+		inimigo[i].vida = 30;
+
+	}
+
+
+
+	//definicoes do tiro
+	ataque.sprite.loadImage("players/armaPlayer.png");
+	ataque.sprite2.loadImage("players/armaPlayerUp1.png");
+	ataque.tamanhoX = ataque.sprite.getHeight() / 2;
+	ataque.tamanhoX = ataque.sprite.getWidth() / 2;
+	ataque.sprite.setAnchorPoint(ataque.sprite.getWidth() / 2, ataque.sprite.getHeight() / 2);
+	ataque.sprite2.setAnchorPoint(ataque.sprite2.getWidth() / 2, ataque.sprite2.getHeight() / 2);
+	ataque.acele = 950.0f;
+	ataque.acompanhando = true;
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+	switch (estadoJogo)
+	{
+	case Menu:
+
+		break;
+
+	case GamePlay:
+
+		if (player.vida > 0)
+		{
+			//pegando tempo para usar na movimentação
+			double time = ofGetLastFrameTime();
+
+			//setando as posicoes x e y mais a velocidade multiplicada pelo tempo
+			player.posicao += player.vel * time;
+
+			//camera do player
+			camera = player.posicao - ofVec2f(ofGetWindowWidth() / 2, ofGetWindowHeight() / 2);
+
+			// velocidade do tiro multiplicada pelo tempo
+			ataque.posicao += ataque.vel * time;
+
+			//setando as posicoes x e y mais a velocidade multiplicada pelo tempo
+			for (int i = 0; i < Ninimigo; i++)
+			{
+				inimigo[i].posicao += inimigo[i].vel * time;
+			}
+
+
+			//travando o player na tela em x,y
+			travaTela(player);
+
+			//se a tecla for pressionada o player moviemnta e atira
+			movimento(player, ataque);
+
+			//colisao com o power up
+			for (int i = 0; i < Ninimigo; i++)
+			{
+				colisaoPowerUp(player, damageUp, inimigo[i]);
+				colisaoPowerUp(player, potion, inimigo[i]);
+			}
+
+			//ALEATORIEDADE DAS FUNCOES
+			for (int i = 0; i < Ninimigo; i++)
+			{
+				if (inimigo[i].vida > 0)
+				{
+					colisao(player, inimigo[i]);
+					if (ataque.Tiro == true)
+					{
+						colisaoTiro(inimigo[i], ataque, player);
+						if (inimigo[i].vida <= 0)
+							sorteioDrop(inimigo[i]);
+					}
+					//Inimigo andando pela tela para cima e baixo
+					movimentoInimigo(inimigo[i], player);
+
+					if (inimigo[i].vida <= 10)
+					{
+						inimigo[i].dano = 4;
+						inimigo[i].acele = 200.0f;
+						monstroSeguir(player, inimigo[i]);
+					}
+				}
+			}
+
+
+		}
+	}
+}
+
+
+//--------------------------------------------------------------
+
+void ofApp::draw()
+{
+
+
+	switch (estadoJogo)
+	{
+	case Menu:
+
+		FundoMenu.draw(0, 0);
+
+		break;
+
+	case GamePlay:
+
+		if (player.vida > 0)
+		{
+
+
+			//fundo do jogo
+			desenhoNaTela(fundo, camera);
+
+			//HUD
+			for (int i = 0; i < player.vida; i++)
+			{
+				
+				player.lifePlayer.draw(10 + (i * player.tamanhoXLife), 80);
+				if (player.vida >= 300)
+					player.vida = 300;
+			}
+
+			player.lifeBarPlayer.draw(10, 80);
+
+			//HUD ENEMYS
+			for (int i = 0; i < Ninimigo; i++)
+			{
+				if (inimigo[i].vida > 0)
+				{
+					for (int j = 0; j < inimigo[i].vida; j++)
+					{
+						inimigo[i].EnemyLife.draw(((inimigo[i].posicao.x + (j * 3)) - 60) - camera.x, (inimigo[i].posicao.y - 70) - camera.y);
+					}
+					inimigo[i].EnemyBar.draw((inimigo[i].posicao.x - 60) - camera.x, (inimigo[i].posicao.y - 70) - camera.y);
+				}
+			}
+
+			//power up desenhado na tela apos o inimigo morrer
+			if (damageUp.Ingame == true)
+				desenhoPowerUpDamage(damageUp, camera);
+			else if (potion.Ingame == true)
+				desenhoPowerUpPotion(potion, camera);
+			
+			desenhoNaTela(player, camera);
+
+			for (int i = 0; i < Ninimigo; i++)
+			{
+				desenhoNaTelaTiro(ataque, camera, inimigo[i]);
+			}
+
+			//Desenhando inimigos
+			for (int i = 0; i < Ninimigo; i++)
+			{
+				if (inimigo[i].vida > 0)
+				{
+					desenhoNaTelaMonstro(inimigo[i], camera);
+				}
+			}
+
+			//Debugs para testes
+			ofDrawBitmapString("Posicao do player: " + ofToString(player.posicao), 10, 10);
+			ofDrawBitmapString("PONTOS: " + ofToString(player.pontos), 10, 50);
+
+		}
+
+		else if (player.vida <= 0)
+			estadoJogo = GameOver;
+
+
+
+		/*if (player.quant > 10)
+		{
+		ofBackground(0, 0, 0);
+		ofDrawBitmapString("VOCE VENCEU!", 400, 300);
+		}*/
+
+		break;
+
+	case GameOver:
+	{
+		ofBackground(0, 0, 0);
+		ofDrawBitmapString("VOCE MORREU!", 400, 300);
+	}
+	break;
+
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
+	switch (estadoJogo)
+	{
+	case Menu:
+		if (key == '1')
+			estadoJogo = GamePlay;
+		break;
+
+	case GamePlay:
+		//se a tecla estiver sendo pressionada, movimenta
+		if (key == OF_KEY_UP || key == 'w')
+		{
+			player.Up = true;
+		}
+		if (key == OF_KEY_DOWN || key == 's')
+		{
+			player.Down = true;
+		}
+		if (key == OF_KEY_RIGHT || key == 'd')
+		{
+			player.Right = true;
+			if (ataque.Tiro == false) {
+				ataque.acele = 950.0f;
+			}
+		}
+		if (key == OF_KEY_LEFT || key == 'a')
+		{
+			player.Left = true;
+			if (ataque.Tiro == false) {
+				ataque.acele = -950.0f;
+			}
+		}
+		//tiro do personagem ativado e moviementa
+		if (key == 'x' || key == 'X')
+		{
+			ataque.acompanhando = false;
+			ataque.Tiro = true;
+
+		}
+		break;
+	case GameOver:
+		break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key)
+{
+	switch (estadoJogo)
+	{
+	case Menu:
+		break;
+	case GamePlay:
+
+		//se a telca não estiver sendo pressionada, para movimento
+		if (key == OF_KEY_UP || key == 'w')
+		{
+			player.vel.y = 5;
+			player.Up = false;
+		}
+		if (key == OF_KEY_DOWN || key == 's')
+		{
+			player.vel.y = 5;
+			player.Down = false;
+		}
+		if (key == OF_KEY_RIGHT || key == 'd')
+		{
+			player.vel.x = 0;
+			player.Right = false;
+		}
+		if (key == OF_KEY_LEFT || key == 'a')
+		{
+			player.vel.x = 0;
+			player.Left = false;
+		}
+		if (key == 'x')
+		{
+			ataque.acompanhando = true;
+			ataque.Tiro = false;
+		}
+		break;
+
+	case GameOver:
+		break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
---
+++ oCopiafApp.h
@@ -0,0 +1,367 @@
+#pragma once
+
+
+#include "ofMain.h"
+#define Ninimigo 10
+#define Menu 1
+#define GamePlay 2
+#define GameOver 3
+
+
+	class ofApp : public ofBaseApp
+{
+
+
+public:
+
+	void setup();
+	void update();
+	void draw();
+
+
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+
+	//vetor da camera
+	ofVec2f camera;
+
+	//Imagens da HUD
+
+
+	ofImage FundoMenu;
+
+	int estadoJogo = Menu;
+
+	//personagem completo
+	struct personagem
+
+	{
+		ofVec2f posicao;
+		ofVec2f vel;
+		ofImage sprite;
+		ofImage sprite2;
+		ofImage lifeBarPlayer;
+		ofImage lifePlayer;
+		float tamanhoX, tamanhoY, tamanhoXLife, tamanhoYLife;
+		float acele;
+		int vida, pontos = 0, dano = 1;
+		bool Up = false; bool Down = false; bool Right = false; bool Left = false; bool Tiro = false;
+		bool acompanhando = false; bool tiroDirecao = false; bool colison = false;
+
+	};
+	personagem player;
+	personagem fundo;
+	
+	struct powerUp
+	{
+		bool Ingame; bool moredamage = false; bool cure = false;
+		int dano = 3;
+		int vida = rand() % 3;
+		float tamanhoX, tamanhoY;
+		ofVec2f posicao;
+		ofImage damage;
+		ofImage heal;
+	};
+	powerUp damageUp;
+	powerUp potion;
+
+	struct monstros
+
+	{
+		ofVec2f posicao;
+		ofVec2f vel;
+		ofVec2f path;
+		ofImage sprite;
+		ofImage sprite2;
+		ofImage sprite3;
+		ofImage EnemyBar;
+		ofImage EnemyLife;
+		float tamanhoX, tamanhoY, tamanhoXLife, tamanhoYLife;
+		float acele;
+		int vida, dano = 2;
+		bool IniSub = false;
+		bool IniDesc = false;
+		bool IniDir = false; bool IniEsq = false; bool atingido = false;
+		bool powerUpActive = false;
+	};
+
+
+	monstros inimigo[Ninimigo];
+
+
+	//
+	struct golpes
+	{
+		ofVec2f posicao;
+		ofVec2f vel;
+		ofImage sprite;
+		ofImage sprite2;
+		float tamanhoX, tamanhoY;
+		float acele;
+		bool Tiro = false;
+		bool acompanhando = false; bool Dir = false;
+		bool powerUpAtkActive = false;
+	};
+	//preciso mudar esse nome de variavel
+	golpes ataque;
+
+
+
+
+	void desenhoNaTela(personagem& objeto, ofVec2f& mundo)
+	{
+		objeto.sprite.draw(objeto.posicao - mundo);
+	}
+	void desenhoPowerUpDamage(powerUp& objeto, ofVec2f mundo)
+	{
+		 objeto.damage.draw(objeto.posicao - mundo);
+	}
+	void desenhoPowerUpPotion(powerUp& objeto, ofVec2f mundo)
+	{
+		objeto.heal.draw(objeto.posicao - mundo);
+	}
+	void desenhoNaTelaTiro(golpes& objeto, ofVec2f& mundo, monstros& monstro)
+	{
+		if (monstro.powerUpActive == false)
+			objeto.sprite.draw(objeto.posicao - mundo);
+		else
+			objeto.sprite2.draw(objeto.posicao - mundo);
+	}
+	void desenhoNaTelaMonstro(monstros& inimigo, ofVec2f& mundo)
+	{
+
+		if (inimigo.atingido == false)
+			inimigo.sprite.draw(inimigo.posicao - mundo);
+		else
+			inimigo.sprite2.draw(inimigo.posicao - mundo);
+		if (inimigo.vida <= 10)
+			inimigo.sprite3.draw(inimigo.posicao - mundo);
+	}
+
+
+	//FUNCAO DE COLISAO QUE PASSA O PLAYER E O QUE COLIDE
+	void colisao(personagem& P1, monstros& P2)
+	{
+
+		//Colisao certa
+		if (P1.posicao.x > P2.posicao.x - P2.tamanhoX && P1.posicao.x < P2.posicao.x + P2.tamanhoX &&
+			P1.posicao.y + P1.tamanhoY > P2.posicao.y - P2.tamanhoY &&
+			P1.posicao.y - P1.tamanhoY < P2.posicao.y + P2.tamanhoY)
+		{
+			//Se colidir com objeto volta uma posicao em y
+			P1.posicao -= 2;
+			P1.vida -= P2.dano;
+		}
+	}
+
+	//colisao com powerUp
+	void colisaoPowerUp(personagem& P1, powerUp& P2, monstros& mosntro)
+	{
+
+		//Colisao certa
+		if (P1.posicao.x > P2.posicao.x - P2.tamanhoX && P1.posicao.x < P2.posicao.x + P2.tamanhoX &&
+			P1.posicao.y + P1.tamanhoY > P2.posicao.y - P2.tamanhoY &&
+			P1.posicao.y - P1.tamanhoY < P2.posicao.y + P2.tamanhoY)
+		{
+			//tiro.powerUpAtkActive = false;
+			P2.Ingame = false;
+			mosntro.powerUpActive = true;
+			if (P2.moredamage == true)
+				P1.dano = P2.dano;
+			else if (P2.cure == true)
+				P1.vida += P2.vida;
+
+			
+		}
+	}
+	void colisaoTiro(monstros& T1, golpes& P1, personagem& P2)
+	{
+
+		//Colisao certa
+		if (P1.posicao.x > T1.posicao.x - T1.tamanhoX && P1.posicao.x < T1.posicao.x + T1.tamanhoX &&
+			P1.posicao.y + P1.tamanhoY > T1.posicao.y - T1.tamanhoY &&
+			P1.posicao.y - P1.tamanhoY < T1.posicao.y + T1.tamanhoY)
+		{
+			//Se colidir com objeto aumenta pontos, tira vida dos monstros e ativa a I.A 
+			P2.pontos += 5;
+			P2.colison = true;
+			T1.vida -= P2.dano;
+			T1.atingido = true;
+			T1.IniDir = true;
+		}
+	}
+	//funcao que o inimigo segue o player 
+	void monstroSeguir(personagem jogador, monstros&  monstro)
+	{
+		monstro.path = jogador.posicao - monstro.posicao;
+		monstro.path.normalize();
+		monstro.vel += monstro.acele + abs(monstro.vel.x) + abs(monstro.vel.y);
+		monstro.vel = monstro.path * monstro.vel.length();
+	}
+
+	void sorteioDrop(monstros& inimigo)
+	{
+		int sorteio = rand() % 200;
+		
+		if (sorteio < 50)
+		{
+			inimigo.powerUpActive = true;
+			damageUp.posicao = inimigo.posicao;
+			damageUp.Ingame = true;
+			damageUp.moredamage = true;
+		}
+		else if (sorteio > 50 && sorteio < 150)
+		{
+			inimigo.powerUpActive = false;
+			potion.posicao = inimigo.posicao;
+			potion.Ingame = true;
+			potion.cure = true;
+		}
+	}
+	void travaTela(personagem& P1)
+	{
+		//travando em y
+		if (P1.posicao.y > 1100)
+		{
+			
+			P1.acele = 0.1f;
+			//P1.posicao.y -= 4;
+		}
+		else if (P1.posicao.y < 400)
+		{
+			
+			P1.vel.limit(0);
+			//P1.posicao.y += 4;
+		}
+		//travando o player na tela em x
+		if (P1.posicao.x < 300)
+		{
+			
+			P1.vel.limit(0);
+			//P1.posicao.x += 4;
+		}
+	}
+
+
+	void movimento(personagem& P1, golpes& T1)
+	{
+		if (P1.Up == true)
+		{
+			P1.vel.y -= P1.acele;
+		}
+		//movimenta para baixo
+		if (P1.Down == true)
+		{
+			P1.vel.y += P1.acele;
+		}
+		//moviemnta para esquerda
+		if (P1.Left == true)
+		{
+
+			P1.vel.x -= P1.acele;
+		}
+		//movimenta para direita (e so atira para direita)
+		if (P1.Right == true)
+		{
+			P1.vel.x += P1.acele;
+		}
+
+		P1.vel.limit(500);
+		
+		
+		//Tiro sempre segue o player e se colidir com inimigo volta
+		if (T1.acompanhando == true || P1.colison == true)
+		{
+			T1.posicao = P1.posicao;
+			P1.colison = false;
+		}
+		//tiro movimenta se pressionado x
+		else if (T1.Tiro == true)
+		{
+			T1.vel.x = T1.acele;
+		}
+	}
+
+	void movimentoInimigo(monstros &inimigo, personagem& player)
+	{
+		inimigo.vel.limit(100);
+		//movimento padrao para cima e para baixo
+		if (inimigo.atingido == false && inimigo.posicao.distance(player.posicao) > 400)
+		{
+			if (inimigo.IniSub == true)
+			{
+
+				inimigo.vel.y -= inimigo.acele;
+				if (inimigo.posicao.y < 400)
+				{
+					inimigo.vel.set(1, 1);
+					inimigo.IniSub = false;
+					inimigo.IniDesc = true;
+				}
+			}
+			if (inimigo.IniDesc == true)
+			{
+
+				inimigo.vel.y += inimigo.acele;
+				if (inimigo.posicao.y > 1100)
+				{
+					inimigo.vel.set(1, 1);
+					inimigo.IniSub = true;
+					inimigo.IniDesc = false;
+				}
+			}
+		}
+		else if (inimigo.atingido == false)
+		{
+			monstroSeguir(player, inimigo);
+		}
+
+		//Se o inimigo levar um tiro ele muda a I.A
+		else
+		{
+			inimigo.acele = 10.0f;
+
+			if (inimigo.posicao.y < 400 || inimigo.posicao.y > 1100)
+			{
+				inimigo.atingido = false;
+			}
+
+			if (inimigo.IniDir == true)
+			{
+				inimigo.vel.y = 0;
+				inimigo.vel.x += inimigo.acele;
+				if (inimigo.posicao.x > 4000)
+				{
+
+					inimigo.vel.set(1, 1);
+					inimigo.IniDir = false;
+					inimigo.IniEsq = true;
+				}
+			}
+			if (inimigo.IniEsq == true)
+			{
+				inimigo.vel.y = 0;
+				inimigo.vel.x -= inimigo.acele;
+				if (inimigo.posicao.x < 10)
+				{
+
+					inimigo.vel.set(1, 1);
+					inimigo.IniDir = true;
+					inimigo.IniEsq = false;
+				}
+			}
+		}
+	}
+
+};
